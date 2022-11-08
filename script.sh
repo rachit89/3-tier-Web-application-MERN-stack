9c84d2cbded242ed510770dfcbab2423b0a28fc7
rachit89 <115970922+rachit89@users.noreply.github.com>
2022-11-08 16:40:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove --purge
 sudo apt autoclean
-sudo snap install node --classic
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install nodejs
 sudo npm install -g npm@latest
 sudo npm install -g pm2@latest
 git clone https://github.com/rachit89/node-express-realworld-example-app.git
@@ -15,5 +16,4 @@ sudo chown ubuntu:ubuntu -R /opt
 mkdir /opt/softwares
 sudo wget https://s3.us-east-2.amazonaws.com/amazoncloudwatch-agent-us-east-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-sudo apt install -y awscli
-aws ssm send-command --document-name "AmazonCloudWatch-ManageAgent" --document-version "8" --parameters '{"action":["configure"],"mode":["ec2"],"optionalConfigurationSource":["ssm"],"optionalConfigurationLocation":["AmazonCloudWatch-linux-rachit-nodejs"],"optionalOpenTelemetryCollectorConfigurationSource":["ssm"],"optionalOpenTelemetryCollectorConfigurationLocation":[""],"optionalRestart":["yes"]}' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region us-east-2
+sudo snap install aws-cli --classic
